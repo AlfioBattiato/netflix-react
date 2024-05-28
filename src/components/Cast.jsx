8c2880fb28eae6fc1a1d46bd43c7771ebfba1033
ALFIO BATTIATO <battiato3@gmail.com>
2024-05-29 00:23:14 +0200

    avatar

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -10,13 +10,13 @@ function Cast(props) {
             <div onMouseEnter={() => { setShow('') }}
                 onMouseLeave={() => { setShow('d-none') }}>
                 <img
-                    src={props.img ? `https://image.tmdb.org/t/p/original/${props.img}.webp` : "/assets/avatar.jpg"}
+                    src={props.img ? `https://image.tmdb.org/t/p/original/${props.img}.webp` : "/assets/Missing_photo.svg"}
                     alt="img"
                     width={"90%"}
                     height={"100%"}
 
                 />
-                <div className={` dk3  m-0 castAnimation  ${show}`} style={{ width: "90%", height: "auto" }}>
+                <div className={` dk3 pb-4 px-1 m-0 castAnimation  ${show}`} style={{ width: "90%", height: "auto" }}>
 
                     <span>Role:</span>
                     <p className="text-blue fw-bold "  >{props.character}</p>
